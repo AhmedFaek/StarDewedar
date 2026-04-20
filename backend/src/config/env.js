e8cedb701f75647019a52badcf53822c06fea7a8
Ahmed <ahmed22012501@gmail.com>
2026-04-20 14:37:07 +0200

    cloudinary configurations

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -7,6 +7,9 @@ const requiredEnv = [
     'DATABASE_URL',
     'JWT_SECRET',
     'JWT_REFRESH_SECRET',
+    'CLOUDINARY_NAME',
+    'CLOUDINARY_KEY',
+    'CLOUDINARY_SECRET',
 ]
 
 // Validate required variables
@@ -21,4 +24,7 @@ export default {
     databaseUrl: process.env.DATABASE_URL,
     jwtSecret: process.env.JWT_SECRET,
     jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
+    cloudinaryName: process.env.CLOUDINARY_NAME,
+    cloudinaryKey: process.env.CLOUDINARY_KEY,
+    cloudinarySecret: process.env.CLOUDINARY_SECRET,
 }
