6f1f02d9ed9c0cce029afae1abf02b285edaf1ce
Ahmed <ahmed22012501@gmail.com>
2026-05-05 22:32:22 +0300

    Update roles

--- backend/src/utils/constants.js
+++ backend/src/utils/constants.js
@@ -1,5 +1,4 @@
 export const ROLES = {
-    CO_FOUNDER: 'co-founder',
-    PROJECT_MANAGER: 'project-manager',
-    ACCOUNT_MANAGER: 'account-manager',
-}
+    ADMIN: 'admin',
+    CUSTOMER: 'customer',
+}
