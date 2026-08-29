3c58efbfd6204b6ebc68bb74044ba4cc0d11ed70
Ahmed <ahmed22012501@gmail.com>
2026-08-30 02:32:07 +0300

    FIX: Body Size Limit

--- backend/src/app.js
+++ backend/src/app.js
@@ -48,8 +48,8 @@ const resolveErrorStatus = (err) => {
 
 // Middlewares
 app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ limit: '1mb' }))
+app.use(express.urlencoded({ extended: true, limit: '1mb' }))
 
 // Routes
 app.use('/api/auth', authRoutes)
