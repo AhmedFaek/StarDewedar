52e269bec1c10fdb456c312be4e59d708496b7ba
Ahmed <ahmed22012501@gmail.com>
2026-09-17 20:58:26 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,25 @@
 import { lazy, Suspense } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom'
 import { CompareProvider } from './utils/compareContext'
 import CompareDrawer from './components/shared/CompareDrawer'
 import WhatsAppFloat from './components/shared/WhatsAppFloat'
 import TopProgressBar from './components/shared/TopProgressBar'
 import { NotificationProvider } from './context/NotificationContext'
 import ToastContainer from './components/notifications/ToastContainer'
+
+function RedirectQuote() {
+  const location = useLocation()
+  const params = new URLSearchParams(location.search)
+  params.set('type', 'quote')
+  return <Navigate to={`/request?${params.toString()}`} replace />
+}
+
+function RedirectVisit() {
+  const location = useLocation()
+  const params = new URLSearchParams(location.search)
+  params.set('type', 'visit')
+  return <Navigate to={`/request?${params.toString()}`} replace />
+}
 
 const Home = lazy(() => import('./pages/Home'))
 const Products = lazy(() => import('./pages/Products'))
@@ -14,8 +28,7 @@ const Projects = lazy(() => import('./pages/Projects'))
 const ProjectDetail = lazy(() => import('./pages/ProjectDetail'))
 const About = lazy(() => import('./pages/About'))
 const Contact = lazy(() => import('./pages/Contact'))
-const RequestQuote = lazy(() => import('./pages/RequestQuote'))
-const RequestVisit = lazy(() => import('./pages/RequestVisit'))
+const Request = lazy(() => import('./pages/Request'))
 const SavedProducts = lazy(() => import('./pages/SavedProducts'))
 const ComparePage = lazy(() => import('./pages/ComparePage'))
 const ResetPassword = lazy(() => import('./pages/ResetPassword'))
@@ -33,8 +46,9 @@ export default function App() {
             <Route path="/project-detail" element={<ProjectDetail />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="/request-quote" element={<RequestQuote />} />
-            <Route path="/request-visit" element={<RequestVisit />} />
+            <Route path="/request" element={<Request />} />
+            <Route path="/request-quote" element={<RedirectQuote />} />
+            <Route path="/request-visit" element={<RedirectVisit />} />
             <Route path="/saved-products" element={<SavedProducts />} />
             <Route path="/compare" element={<ComparePage />} />
             <Route path="/reset-password" element={<ResetPassword />} />
